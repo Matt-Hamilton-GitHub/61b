b1905a8182a9de0f0e4e6bcc08597a0840e4a775
Matt <matt.hamilton@berkeley.edu>
2023-02-17 17:05:26 -0800

    iterator implemented

--- ArraySet/src/ArraySet.java
+++ ArraySet/src/ArraySet.java
@@ -1,4 +1,8 @@
-public class ArraySet<T> {
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ArraySet<T> implements Iterable<T>{
     private T[] items;
     private int size;
 
@@ -7,14 +11,58 @@ public class ArraySet<T> {
         size = 0;
     }
 
+    public Iterator<T> iterator(){
+
+        return new ArraySetIterator();
+    }
+
+    private class ArraySetIterator implements  Iterator<T>{
+        private int wizPos;
+        public ArraySetIterator(){
+            wizPos = 0;
+        }
+
+        public boolean hasNext(){
+            return wizPos < size;
+        }
+        public T next(){
+            T returnItem = items[wizPos];
+            wizPos++;
+            return returnItem;
+        }
+
+    }
     public static void main(String[] args) {
 
         ArraySet<String> s = new ArraySet<>();
+        Set<Integer> javaset = new HashSet<>();
 
         s.add(null);
         s.add("horse");
         s.add("fish");
 
+        javaset.add(5);
+        javaset.add(23);
+        javaset.add(42);
+
+        Iterator<Integer> seer = javaset.iterator();
+
+        while (seer.hasNext()){
+            int i = seer.next();
+            System.out.println(i);
+        }
+        Iterator<String> aseer = s.iterator();
+
+        while (aseer.hasNext()){
+            String i = aseer.next();
+            System.out.println(i);
+        }
+
+        for(String i : s){
+            System.out.println(i);
+        }
+
+        System.out.println(s.toString());
     }
 
     public boolean contains(T x) {
@@ -38,5 +86,15 @@ public class ArraySet<T> {
         size++;
     }
 
+    @Override
+    public String toString(){
+        StringBuilder returnSb = new StringBuilder("{");
+        for (T item : this){
+            returnSb.append(item);
+            returnSb.append(", ");
+        }
+        returnSb.append("}");
+        return returnSb.toString();
+    }
 
 }
